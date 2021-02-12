0a4f70e4027a9b2561b4adf881f24dff5c980a7b
Boris Lachev <boris.lachevbg00@gmail.com>
2021-02-12 20:10:09 +0200

    Add SQL error parsing

--- common/errors.go
+++ common/errors.go
@@ -2,12 +2,17 @@ package common
 
 import (
 	"errors"
+	"github.com/go-sql-driver/mysql"
 	"net/http"
 )
 
+const DUPLICATE_ENTRY = 1062
+
 var (
-	InvalidModelError   = errors.New("invalid model")
-	EntityNotFoundError = errors.New("entity not found")
+	InvalidModelError     = errors.New("invalid model")
+	EntityNotFoundError   = errors.New("entity not found")
+	DuplicateEntityError  = errors.New("duplicate entity by unique column")
+	PasswordTooShortError = errors.New("password is too short")
 )
 
 type HttpResponse struct {
@@ -28,9 +33,34 @@ func GetErrorResponse(err error) (int, HttpResponse) {
 			Body: err.Error(),
 		}
 	default:
+		me, ok := err.(*mysql.MySQLError)
+		if ok {
+			domainErr := ParseMySQLError(me)
+			switch domainErr {
+			case DuplicateEntityError:
+				return http.StatusBadRequest, HttpResponse{
+					Code: http.StatusBadRequest,
+					Body: domainErr.Error(),
+				}
+			default:
+				return http.StatusInternalServerError, HttpResponse{
+					Code: http.StatusInternalServerError,
+					Body: domainErr.Error(),
+				}
+			}
+		}
 		return http.StatusInternalServerError, HttpResponse{
 			Code: http.StatusInternalServerError,
 			Body: err.Error(),
 		}
+	}
+}
+
+func ParseMySQLError(sqlError *mysql.MySQLError) error {
+	switch sqlError.Number {
+	case DUPLICATE_ENTRY:
+		return DuplicateEntityError
+	default:
+		return sqlError
 	}
 }
