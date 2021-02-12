786a3ef30efb6971923e9240dc6b3b63ac4d7a58
Boris Lachev <boris.lachevbg00@gmail.com>
2021-02-12 14:21:26 +0200

    Add service layer

---
+++ service/recipe.go
@@ -0,0 +1,93 @@
+package service
+
+import (
+	"gorest/db"
+	"gorest/delivery/models"
+	"gorest/entity"
+	"gorm.io/gorm"
+)
+
+type Recipe interface {
+	FindAll() ([]entity.Recipe, error)
+	FindAllPagedAndSorted(
+		pageNumber int,
+		pageSize int,
+		sortingAttribute string,
+		ascending bool) ([]entity.Recipe, error)
+	FindByID(id uint) (entity.Recipe, error)
+	FindAllByTitle(str string) ([]entity.Recipe, error)
+	FindAllByProducts(products *entity.Products) ([]entity.Recipe, error)
+	FindAllByTags(tags *entity.Tags) ([]entity.Recipe, error)
+	Create(recipe *entity.Recipe) error
+	CreateBatch(recipes *[]entity.Recipe) error
+	Update(user *entity.Recipe) error
+	DeleteByID(recipeID uint) (entity.Recipe, error)
+	Count() (int, error)
+	Search(req models.RecipesSearchRequest) ([]entity.Recipe, error)
+}
+
+type RecipeService struct {
+	recipeRepository db.RecipeRepository
+	userRepository   db.UserRepository
+}
+
+func NewRecipeService(recipeRepo *db.RecipeRepository, userRepo *db.UserRepository) *RecipeService {
+	return &RecipeService{
+		recipeRepository: *recipeRepo,
+		userRepository:   *userRepo,
+	}
+}
+
+type RecipeRepository struct {
+	db *gorm.DB
+}
+
+// External r.recipeRepository methods
+func (r *RecipeService) FindAll() ([]entity.Recipe, error) {
+	return r.recipeRepository.FindAll()
+}
+
+func (r *RecipeService) FindAllPagedAndSorted(pageNumber int, pageSize int, sortingAttribute string, ascending bool) ([]entity.Recipe, error) {
+	return r.recipeRepository.FindAllPagedAndSorted(pageSize, pageSize, sortingAttribute, ascending)
+}
+
+func (r *RecipeService) FindByID(id uint) (entity.Recipe, error) {
+	return r.recipeRepository.FindByID(id)
+}
+
+func (r *RecipeService) FindAllByTitle(title string) ([]entity.Recipe, error) {
+	return r.recipeRepository.FindAllByTitle(title)
+}
+
+func (r *RecipeService) FindAllByProducts(products *entity.Products) ([]entity.Recipe, error) {
+	return r.recipeRepository.FindAllByProducts(products)
+}
+
+func (r *RecipeService) FindAllByTags(tags *entity.Tags) ([]entity.Recipe, error) {
+	return r.recipeRepository.FindAllByTags(tags)
+}
+
+func (r *RecipeService) Create(recipe *entity.Recipe) error {
+	return r.recipeRepository.Create(recipe)
+}
+
+func (r *RecipeService) CreateBatch(recipes *[]entity.Recipe) error {
+	return r.recipeRepository.CreateBatch(recipes)
+}
+
+func (r *RecipeService) Update(recipe *entity.Recipe) error {
+	return r.recipeRepository.Update(recipe)
+}
+
+func (r *RecipeService) DeleteByID(recipeID uint) (entity.Recipe, error) {
+	return r.recipeRepository.DeleteByID(recipeID)
+}
+
+func (r *RecipeService) Count() (int, error) {
+	return r.recipeRepository.Count()
+}
+
+// Service methods
+func (r *RecipeService) Search(req models.RecipesSearchRequest) ([]entity.Recipe, error) {
+	return nil, nil
+}
---
+++ service/user.go
@@ -0,0 +1,97 @@
+package service
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"gorest/db"
+	"gorest/entity"
+)
+
+type User interface {
+	FindAll() ([]entity.User, error)
+	FindAllPagedAndSorted(
+		pageNumber int,
+		pageSize int,
+		sortingAttribute string,
+		ascending bool) ([]entity.User, error)
+	FindByID(id uint) (entity.User, error)
+	FindByUsername(username string) (entity.User, error)
+	Create(user *entity.User) error
+	Update(user *entity.User) error
+	DeleteByID(userID uint) error
+	Count() (int, error)
+	Login(username string, password string) (entity.User, error)
+}
+
+type UserService struct {
+	userRepository db.UserRepository
+}
+
+func NewUserService(userRepo *db.UserRepository) *UserService {
+	return &UserService{
+		userRepository: *userRepo,
+	}
+}
+
+func (r *UserService) FindAll() ([]entity.User, error) {
+	return r.userRepository.FindAll()
+}
+
+func (r *UserService) FindAllPagedAndSorted(pageNumber int, pageSize int, sortingAttribute string, ascending bool) ([]entity.User, error) {
+	return r.userRepository.FindAllPagedAndSorted(pageNumber, pageSize, sortingAttribute, ascending)
+}
+
+func (r *UserService) FindByID(id uint) (entity.User, error) {
+	return r.userRepository.FindByID(id)
+}
+
+func (r *UserService) FindByUsername(username string) (entity.User, error) {
+	return r.userRepository.FindByUsername(username)
+}
+
+func (r *UserService) Create(user *entity.User) error {
+	if len(user.Password) <= 8 {
+		return errors.New("password is too short")
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	str := string(password)
+	user.Password = str
+
+	return r.userRepository.Create(user)
+}
+
+func (r *UserService) Update(user *entity.User) error {
+	existingUser, err := r.FindByID(user.ID)
+	if err != nil {
+		return err
+	}
+
+	if existingUser.Username != user.Username {
+		return errors.New("can't change username")
+	}
+
+	fail := bcrypt.CompareHashAndPassword([]byte(existingUser.Password), []byte(user.Password))
+	if fail != nil {
+		newPass, _ := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+		existingUser.Password = string(newPass)
+	}
+
+	existingUser.Gender = user.Gender
+	existingUser.Role = user.Role
+	existingUser.AvatarUrl = user.AvatarUrl
+	existingUser.Description = user.Description
+	existingUser.Valid = user.Valid
+	existingUser.Recipes = user.Recipes
+
+	return r.userRepository.Update(&existingUser)
+}
+
+func (r *UserService) DeleteByID(userID uint) error {
+	// TODO rework
+	return r.userRepository.DeleteByID(userID)
+}
+
+func (r *UserService) Count() (int, error) {
+	return r.userRepository.Count()
+}
