c4dfb6309115b35255b084a78c6704402e762e87
Boris Lachev <boris.lachevbg00@gmail.com>
2021-02-12 20:09:41 +0200

    Update entity models

--- entity/recipe.go
+++ entity/recipe.go
@@ -2,18 +2,17 @@ package entity
 
 import (
 	"database/sql/driver"
-	"errors"
+	"github.com/dystopia-systems/alaskalog"
 	"github.com/go-sql-driver/mysql"
 	"strings"
 	"time"
 )
 
 type Recipe struct {
-	ID               uint           `gorm:"primarykey" json:"id;omitEmpty"`
-	CreatedAt        time.Time      `json:"created_at;omitEmpty"`
-	UpdatedAt        time.Time      `json:"updated_at;omitEmpty"`
-	DeletedAt        mysql.NullTime `gorm:"index" json:"deleted_at;omitEmpty"`
-	UserID           uint           `gorm:"not null" json:"user_id"`
+	ID               uint           `gorm:"primarykey" json:"id"`
+	CreatedAt        time.Time      `json:"created_at"`
+	UpdatedAt        time.Time      `json:"updated_at"`
+	DeletedAt        mysql.NullTime `gorm:"index" json:"deleted_at"`
 	Title            string         `gorm:"not null; size:80" json:"title"`
 	ShortDescription string         `gorm:"not null; size:256" json:"short_description"`
 	TimeToCookNs     time.Duration  `gorm:"not null" json:"time_to_cook_ns"`
@@ -21,17 +20,17 @@ type Recipe struct {
 	ImageUrl         string         `gorm:"not null" json:"image_url"`
 	Description      string         `gorm:"not null;size:2048" json:"description"`
 	Tags             Tags           `gorm:"type:text" json:"tags"`
+	User             User           `json:"-"`
+	UserID           uint           `gorm:"not null" json:"-"`
 }
 
 type Products []string
 type Tags []string
 
 func (p *Products) Scan(src interface{}) error {
-	str, ok := src.(string)
-	if !ok {
-		return errors.New("source is not string")
-	}
+	str, _ := src.(string)
 	*p = strings.Split(str, ",")
+	alaskalog.Logger.Println(*p)
 	return nil
 }
 
@@ -43,10 +42,7 @@ func (p Products) Value() (driver.Value, error) {
 }
 
 func (t *Tags) Scan(src interface{}) error {
-	str, ok := src.(string)
-	if !ok {
-		return errors.New("source is not string")
-	}
+	str, _ := src.(string)
 	*t = strings.Split(str, ",")
 	return nil
 }
--- entity/user.go
+++ entity/user.go
@@ -9,10 +9,10 @@ type Gender int
 type Role string
 
 type User struct {
-	ID          uint         `gorm:"primarykey" json:"id;omitEmpty"`
-	CreatedAt   time.Time    `gorm:"not null" json:"created_at;omitEmpty"`
-	UpdatedAt   time.Time    `json:"updated_at;omitEmpty"`
-	DeletedAt   sql.NullTime `gorm:"index" json:"deleted_at;omitEmpty"`
+	ID          uint         `gorm:"primarykey" json:"id"`
+	CreatedAt   time.Time    `gorm:"not null" json:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at"`
+	DeletedAt   sql.NullTime `gorm:"index" json:"deleted_at"`
 	Username    string       `gorm:"unique;not null;size:15;" json:"username"`
 	Password    string       `gorm:"not null" json:"password"`
 	Gender      Gender       `gorm:"not null" json:"gender"`
@@ -20,5 +20,7 @@ type User struct {
 	AvatarUrl   string       `gorm:"not null" json:"avatar_url"`
 	Description string       `gorm:"not null;size:512" json:"description"`
 	Valid       bool         `gorm:"not null" json:"valid"`
-	Recipes     []Recipe     `json:"recipes;omitEmpty"`
+	Recipes     Recipes      `gorm:"ForeignKey:UserID" json:"recipes"`
 }
+
+type Recipes []Recipe
