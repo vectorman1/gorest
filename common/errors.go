34b1e8523a11812b626c56f4c08d3904cfe61cdd
Boris Lachev <boris.lachevbg00@gmail.com>
2021-02-12 14:22:26 +0200

    Add errors

--- common/errors.go
+++ common/errors.go
@@ -1,1 +1,36 @@
 package common
+
+import (
+	"errors"
+	"net/http"
+)
+
+var (
+	InvalidModelError   = errors.New("invalid model")
+	EntityNotFoundError = errors.New("entity not found")
+)
+
+type HttpResponse struct {
+	code int
+	body interface{}
+}
+
+func GetErrorResponse(err error) (int, HttpResponse) {
+	switch err {
+	case InvalidModelError:
+		return http.StatusBadRequest, HttpResponse{
+			code: http.StatusBadRequest,
+			body: err.Error(),
+		}
+	case EntityNotFoundError:
+		return http.StatusNotFound, HttpResponse{
+			code: http.StatusNotFound,
+			body: err.Error(),
+		}
+	default:
+		return http.StatusInternalServerError, HttpResponse{
+			code: http.StatusInternalServerError,
+			body: err.Error(),
+		}
+	}
+}
