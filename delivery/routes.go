35ac0ab12e9174e02036f22f171e2908f7006ff7
Boris Lachev <boris.lachevbg00@gmail.com>
2021-02-12 14:23:09 +0200

    Add new routes.

---
+++ delivery/routes.go
@@ -0,0 +1,12 @@
+package delivery
+
+const Users = "/users"
+const UsersId = "/users/:id"
+const UsersSearch = "/users/search"
+const UsersCount = "/users/count"
+
+const Recipes = "/recipes"
+const RecipesId = "/recipes/:id"
+const RecipesSearch = "/recipes/search"
+const RecipesBatch = "/recipes/batch"
+const RecipesCount = "/recipes/count"
--- web/routes.go
+++
@@ -1,6 +0,0 @@
-package web
-
-const Users = "/users"
-const UsersId = "/users/:id"
-const UsersSearch = "/users/search"
-const UsersCount = "/users/count"
